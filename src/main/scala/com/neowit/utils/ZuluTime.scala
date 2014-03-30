f501a0f5c58d662b8861a36519e9ee1d0c121393
A. G <catch+github@neowit.com>
2014-03-30 19:46:52 +0100

    fix bug - SimpleDateFormat initialisation as instance variable does not work for multi threaded cases

--- src/main/scala/com/neowit/utils/ZuluTime.scala
+++ src/main/scala/com/neowit/utils/ZuluTime.scala
@@ -4,12 +4,14 @@ import java.util.{GregorianCalendar, Calendar, TimeZone}
 import com.sforce.ws.bind.CalendarCodec
 
 object ZuluTime {
-    val dateFormatGmt = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-    dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"))
 
     val codec = new CalendarCodec()
 
     def formatDateGMT(cal: Calendar):String = {
+        //it is tempting to extract SimpleDateFormat initialisation as instance variable, but DO NOT,
+        // as this will not work in multi threaded cases
+        val dateFormatGmt = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"))
         dateFormatGmt.format(cal.getTime) + ".000Z"
     }
     def deserialize(dateStr: String): Calendar = codec.deserialize(dateStr)
