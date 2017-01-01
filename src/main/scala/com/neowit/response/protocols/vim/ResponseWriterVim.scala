43f7c0bd24f23a124e2afa2e5fac273f6b675c08
A. G <catch+github@neowit.com>
2017-01-01 23:21:36 +0000

    updated copyright header

--- src/main/scala/com/neowit/response/protocols/vim/ResponseWriterVim.scala
+++ src/main/scala/com/neowit/response/protocols/vim/ResponseWriterVim.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2017 Andrey Gavrikov.
+ * this file is part of tooling-force.com application
+ * https://github.com/neowit/tooling-force.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.neowit.response.protocols.vim
 
 import java.io.{File, FileOutputStream, OutputStream, PrintWriter}
@@ -7,10 +26,7 @@ import com.neowit.response._
 import com.neowit.utils.JsonSupport
 
 import spray.json._
-/**
-  * Author: Andrey Gavrikov
-  * Date: 02/01/2017
-  */
+
 class ResponseWriterVim(out: OutputStream, autoFlush: Boolean = true, append: Boolean = false) extends ResponseWriter with JsonSupport {
     private val _writer = new PrintWriter(out, autoFlush)
 
