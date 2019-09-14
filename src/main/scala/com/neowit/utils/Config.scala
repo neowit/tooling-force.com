360f769e0f883c03e6140a936e34a3a6642c1df5
A. G <catch+github@neowit.com>
2019-09-14 09:40:22 +0200

    update to SFDC API 47.0 WSDL-s but make calls as v46
    keeping v46 because productions are not on Winter'20 yet

--- src/main/scala/com/neowit/utils/Config.scala
+++ src/main/scala/com/neowit/utils/Config.scala
@@ -221,7 +221,7 @@ regardless of whether it is also specified in config file or not
 
 class Config(val basicConfig: BasicConfig) extends OAuth2JsonSupport with Logging{
 
-    val apiVersion:Double = 45.0
+    val apiVersion:Double = 46.0
 
     //make BasicConfig methods available in Config
     def load(arglist: List[String]): Unit = {
